987d06719ee6e94bd6d8044782db4335a8e63524
gabrielroot <gabrielfer.s88@gmail.com>
2021-11-30 15:53:12 -0300

    Feature: Ultilizando o bootstrap como fonte principal de estilização

--- web/js/editar.js
+++
@@ -1,6 +0,0 @@
-const btnremovefake = document.querySelector('.btnremove')
-const btnremove = document.querySelector('.remove')
-
-btnremovefake.addEventListener('click', ()=>{
-    btnremove.click()
-})
