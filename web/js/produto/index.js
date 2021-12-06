fac441501ef0e61b63d2aac30970fdfb9e291958
gabrielroot <gabrielfer.s88@gmail.com>
2021-12-05 22:46:20 -0300

    Update: Adicionando efeitos com JQuery

--- web/js/produto/index.js
+++ web/js/produto/index.js
@@ -15,6 +15,25 @@ const animateFloatingAdd = floatingAdd =>{
     floatingAdd.animate({right: '-=10px'}, 100);
     floatingAdd.animate({right: '+=20px'}, 100);
     floatingAdd.animate({right: '-=10px'}, 100);
+
+    floatingAdd.hover(()=>{
+        floatingAdd.css('display', 'flex')
+        floatingAdd.css('alignItems', 'center')
+
+        floatingAdd.css('width', '250px')
+        floatingAdd.css('borderRadius', '10px')
+
+        const p = $(".floatingAdd p")
+        p.attr('class', '')
+        p.css('color', 'white')
+        p.css('fontSize', '1.5em')
+        p.css('margin', '0')
+
+    },()=>{
+        $(".floatingAdd p").attr('class', 'hidden')
+        floatingAdd.css('width', '79px')
+        floatingAdd.css('borderRadius', '100%')
+    })
 }
 
 const animateFlashAlert = alert =>{
